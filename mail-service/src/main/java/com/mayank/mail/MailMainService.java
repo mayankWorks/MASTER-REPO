34015fc2ce406dcbac8ab4617c89826a3dbbdad9
mayankWorks <mayank.shah88@gmail.com>
2015-09-27 23:21:17 +0530

    first

---
+++ mail-service/src/main/java/com/mayank/mail/MailMainService.java
@@ -0,0 +1,10 @@
+package com.mayank.mail;
+
+public class MailMainService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is the first test");
+	}
+
+}
